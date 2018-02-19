b08906eb963afa23720550682e05a31708873745
zannibelle <irissun05@gmail.com>
2018-02-19 16:39:22 -0500

    submit

--- src/main/java/q4_matching/Solution.java
+++ src/main/java/q4_matching/Solution.java
@@ -1,5 +1,8 @@
 package q4_matching;
 
+
+import java.util.LinkedList;
+
 public class Solution {
 
     /*
@@ -7,6 +10,45 @@ public class Solution {
      */
 
     public boolean isValid(String formula) {
+        LinkedList<Character> stack = new LinkedList<>();
+        if (formula.isEmpty()) {
+            return true;
+        }
+
+        //if the first symbol is not open parenthesis return false
+        if (formula.charAt(0) == '(' || formula.charAt(0) == '[' || formula.charAt(0) == '{') {
+            for (int i = 0; i < formula.length(); i++) {
+                if (formula.charAt(i) == '(' || formula.charAt(i) == '[' || formula.charAt(i) == '{') {
+                    stack.addFirst(formula.charAt(i));//open parenthesis, push into stack
+
+                } else if (formula.charAt(i) == ')') {
+                    if(stack.isEmpty() == false) {
+                        if(stack.getFirst() == '(') {
+                            stack.removeFirst();
+                        }
+                    }else{return false;}
+                } else if (formula.charAt(i) == ']') {
+                    if(stack.isEmpty() == false) {
+                        if (stack.getFirst() == '[') {
+                            stack.removeFirst();
+                        }
+                    }else{return false;}
+                }else if (formula.charAt(i) == '}') {
+                    if(stack.isEmpty() == false) {
+                        if (stack.getFirst() == '{') {
+                            stack.removeFirst();
+                        }
+                    }else{return false;}
+                }
+
+            }
+            if (stack.isEmpty()) {// to make sure there is no more open parenthesis left in stack
+                return true;
+            } else {
+                return false;
+            }
+        }
         return false;
+
     }
 }
