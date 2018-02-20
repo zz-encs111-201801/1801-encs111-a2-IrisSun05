19b2449e6be5c100043a6e7ea8f53434781fcd2f
zannibelle <irissun05@gmail.com>
2018-02-20 16:40:04 -0500

    submit

--- src/main/java/q6_fib/Solution.java
+++ src/main/java/q6_fib/Solution.java
@@ -1,7 +1,14 @@
 package q6_fib;
 
 public class Solution {
+
     public int fib(int n) {
+        if(n == 1){
         return 1;
+        }else if(n==2){
+            return 1;
+        }else{
+            return fib(n-2)+fib(n-1);
+        }
     }
 }
