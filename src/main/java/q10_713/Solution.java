a060cd71522d81f27daa298ff4cd90ff0b7cb07c
zannibelle <irissun05@gmail.com>
2018-02-22 18:01:02 -0500

    submit

--- src/main/java/q10_713/Solution.java
+++ src/main/java/q10_713/Solution.java
@@ -12,25 +12,39 @@ import java.util.Arrays;
 public class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         int count = 0;
-        int product;
-
-                for (int i = 0; i < nums.length; i++) {
-                    product = nums[i];
-
-                    if (nums[i] > k) continue;
-                    else count++;
-
-                    for (int j = i+1; j<nums.length; j++) {
-                        product = product * nums[j];
-                        if (product < k) count++;
-                        else break;
+//        int product;
+//
+//                for (int i = 0; i < nums.length; i++) {
+//                    product = nums[i];
+//
+//                    if (nums[i] > k) continue;
+//                    else count++;
+//
+//                    for (int j = i+1; j<nums.length; j++) {
+//                        product = product * nums[j];
+//                        if (product < k) count++;
+//                        else break;
+//                    }
+//                }
+            int leftpointer = 0;
+            int rightpointer = 0;
+            int product =1;
+            if(k==0){
+                return 0;
+            }else{
+                while(rightpointer < nums.length){
+                    product = product * nums[rightpointer];
+                    while(product >=k && leftpointer <= rightpointer){
+                        product /= nums[leftpointer];
+                        leftpointer++;
                     }
+                    count +=rightpointer - leftpointer +1;
+                    rightpointer++;
                 }
-
-
-
+            }
              return count;
         }
+
 
 
     public static void main(String[] args) {
