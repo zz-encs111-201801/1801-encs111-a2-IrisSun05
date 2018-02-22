3b4e776c383f62b647e38d5ab61454d14bbabc62
zannibelle <irissun05@gmail.com>
2018-02-22 16:37:30 -0500

    submit

--- src/main/java/q10_713/Solution.java
+++ src/main/java/q10_713/Solution.java
@@ -7,14 +7,35 @@ package q10_713;
  * 请将代码提交到上面的网址进行测试
  */
 
+import java.util.Arrays;
+
 public class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        return 0;
-    }
+        int count = 0;
+        int product;
+
+                for (int i = 0; i < nums.length; i++) {
+                    product = nums[i];
+
+                    if (nums[i] > k) continue;
+                    else count++;
+
+                    for (int j = i+1; j<nums.length; j++) {
+                        product = product * nums[j];
+                        if (product < k) count++;
+                        else break;
+                    }
+                }
+
+
+
+             return count;
+        }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = {2, 5, 10};
-        System.out.println(solution.numSubarrayProductLessThanK(arr, 10));
+        int[] arr = {6, 2, 5, 10};
+        System.out.println(solution.numSubarrayProductLessThanK(arr, 100));
     }
 }
